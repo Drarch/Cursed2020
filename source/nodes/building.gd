5536ab7899d505c15af5384c7e0b7030acd0e1be
Tomasz Chabora <kobewi4e@gmail.com>
2020-08-22 16:53:45 +0200

    Dachy

--- source/nodes/building.gd
+++ source/nodes/building.gd
@@ -1,6 +1,13 @@
 extends Node2D
 
+onready var roof := $Roof as Sprite
+
+func _ready() -> void:
+	roof.frame = randi() % 48
+
 func increase():
-	var sprite := get_child(get_child_count() - 1).duplicate() as Node2D
+	var sprite := get_child(get_child_count() - 2).duplicate() as Node2D
 	sprite.position.y -= 33
 	add_child(sprite)
+	roof.position.y = sprite.position.y - 45
+	roof.raise()
