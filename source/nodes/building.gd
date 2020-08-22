9428bd3a7cf9e35556a3ce82c0d4faf184966011
Tomasz Chabora <kobewi4e@gmail.com>
2020-08-22 17:20:09 +0200

    Kierunki budynków

--- source/nodes/building.gd
+++ source/nodes/building.gd
@@ -1,13 +1,44 @@
 extends Node2D
 
+const FLOOR_DIRECTIONS := [0, 1, 0, 1, 2, 3, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0 , 1, 0, 1, 0, 1, 0, 1, 2, 3, 0, 0, 1, 3, 2, 2, 3, 0, 1, 2, 3, 0, 1, 0, 0, 1, 1, 0, 1]
+const WALL_DIRECTIONS := [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, -1, -1, -1, -1, -1, -1, -1, -1]
+const ROOF_DIRECTIONS := [-1, -1, -1, -1, 0, 1, 3, 2,0, 1, 3, 2, 0, 1, 3, 2, 0 , 1, 3, 2, 0, 1, 3, 2, 0, 1, 3, 2, 0, 1, 3, 2, 0, 1, 3, 2, 0, 1,3, 2, 0, -1, -1, -1, -1, -1, -1, -1, -1]
+
 onready var roof := $Roof as Sprite
 
+var direction: int
+
 func _ready() -> void:
-	roof.frame = randi() % 48
+	randomize()
+	direction = randi() % 4
+	
+	var frool := randi() % FLOOR_DIRECTIONS.size()
+	while FLOOR_DIRECTIONS[frool] != direction:
+		frool = randi() % FLOOR_DIRECTIONS.size()
+	$Wall.frame = frool
+		
+	roof.frame = ROOF_DIRECTIONS.size()
+	while ROOF_DIRECTIONS[roof.frame] != -1 and ROOF_DIRECTIONS[roof.frame] != direction:
+		roof.frame = randi() % ROOF_DIRECTIONS.size()
 
 func increase():
-	var sprite := get_child(get_child_count() - 2).duplicate() as Node2D
+	var sprite := get_child(get_child_count() - 2).duplicate() as Sprite
 	sprite.position.y -= 33
 	add_child(sprite)
-	roof.position.y = sprite.position.y - 45
+	
+	if get_child_count() == 3:
+		sprite.texture = preload("res://tileset/tileset_buildings_walls.png")
+		sprite.hframes = 8
+		sprite.vframes = 3
+		
+		var wall := randi() % WALL_DIRECTIONS.size()
+		while WALL_DIRECTIONS[wall] != -1 and WALL_DIRECTIONS[wall] != direction and WALL_DIRECTIONS[wall] != opposite(direction):
+			wall = randi() % WALL_DIRECTIONS.size()
+		sprite.frame = wall
+		sprite.position.y -= 44
+	
+	roof.position.y = sprite.position.y
 	roof.raise()
+
+func opposite(i: int) -> int:
+	return [2, 3, 0, 1][i]
