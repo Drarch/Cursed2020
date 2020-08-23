c4d167860439641ec4552f8caceb10bb267cf83a
Tomasz Chabora <kobewi4e@gmail.com>
2020-08-23 13:42:42 +0200

    wincyj powerupuf

--- source/arkanoid/Ball.gd
+++ source/arkanoid/Ball.gd
@@ -1,15 +1,19 @@
 extends KinematicBody2D
 
-var direction := Vector2(1, -1).normalized()
-var started: bool
-var speed := 800
+export(Vector2) var direction = Vector2(1, -1).normalized()
+export var started: bool
+export var speed := 800
 
-var destruction := 1
-var hits := 0
-var explosion := 0
+export var destruction := 1
+export var hits := 0
+export var explosion := 0
 
-signal hit
-signal hit2
+signal hit(ball)
+signal hit2(ball)
+
+func _ready() -> void:
+	connect("hit", get_parent(), "_on_Ball_hit")
+	connect("hit2", get_parent(), "_on_Ball_hit2")
 
 func _physics_process(delta: float) -> void:
 	if not started:
@@ -18,7 +22,7 @@ func _physics_process(delta: float) -> void:
 	var col := move_and_collide(direction * speed * delta)
 	if col:
 		if col.collider.is_in_group("building"):
-			emit_signal("hit")
+			emit_signal("hit", self)
 			col.collider.destroy()
 			
 			hits += 1
@@ -29,8 +33,8 @@ func _physics_process(delta: float) -> void:
 				position += col.remainder
 		elif col.collider.name == "Paddle":
 			direction = direction.bounce(col.normal)
-			emit_signal("hit2")
+			emit_signal("hit2", self)
 			direction = get_parent().up().rotated(position.distance_to(get_parent().paddle.position) * sign(position.x - get_parent().paddle.position.x) / 320 * PI/4)
 		else:
 			direction = direction.bounce(col.normal)
-			emit_signal("hit2")
+			emit_signal("hit2", self)
